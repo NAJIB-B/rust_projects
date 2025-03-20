52b9f6a828abc2801bc5db839b41f3bc55d759fc
NAJIB-B <najibajibasu@gmail.com>
2025-03-20 14:54:43 +0100

    Added functionality to remove staff and list staff from particular department

--- miniOrg/src/main.rs
+++ miniOrg/src/main.rs
@@ -1,14 +1,24 @@
-use std::{io, collections::HashMap};
+use std::{collections::HashMap, io};
 
 fn main() {
-    println!("Welcome to mini Org");
-
-    let mut command: String;
+    println!("welcome to oraganization manager");
+    println!("To add a staff to a department ");
+    println!("Type: Add <staff_name> to <department_name>");
+    println!("example: Add najib to engineering");
+    println!("To remove a staff from a department ");
+    println!("Type: Remove <staff_name> from <department_name>");
+    println!("To get list of staffs in a department");
+    println!("Type: List of <department_name>");
+    println!("To get List of all staffs");
+    println!("Type: get all staffs");
+    println!("To exit");
+    println!("Type: exit");
+    println!("----------- Starting -------------");
 
     let mut company: HashMap<String, Vec<String>> = HashMap::new();
 
     loop {
-        println!("Add staffs to department like this 'Add Sally to Engineering' or input 'get all' to get all staffs or exit to exit");
+        println!("Please input your command");
 
         let mut user_input = String::new();
 
@@ -29,7 +39,6 @@ fn main() {
 
         if word_count == 1 {
             if words[0] == "exit" {
-                command = user_input;
                 println!("Exiting...");
                 break;
             } else {
@@ -38,10 +47,13 @@ fn main() {
             }
         }
 
-        if word_count == 2 {
-            if words[0] == "get" && words[1] == "all" {
-                println!("Getting all users...");
+        if word_count == 3 {
+            if words[0] == "get" && words[1] == "all" && words[2] == "staffs" {
+                println!("Getting all staffs...");
                 get_all_staffs(&mut company);
+            } else if words[0] == "list" && words[1] == "of" {
+                println!("Getting all staffs in {}...", words[2]);
+                get_dept_staff(&mut company, words[2].to_string());
             } else {
                 println!("You inputted a wrong command, please try again");
                 continue;
@@ -52,29 +64,67 @@ fn main() {
             if words[0] == "add" && words[2] == "to" {
                 println!("adding {} to {}...", words[1], words[3]);
                 add_staff(&mut company, words[1].to_string(), words[3].to_string());
+            } else if words[0] == "remove" && words[2] == "from" {
+                println!("removing {} to {}...", words[1], words[3]);
+                remove_staff(&mut company, words[1].to_string(), words[3].to_string());
             } else {
                 println!("You inputted a wrong command, please try again");
                 continue;
             }
         } else {
-                println!("You inputted a wrong command, please try again");
-                continue;
+            println!("You inputted a wrong command, please try again");
+            continue;
         }
     }
-    println!("command received: {}", command);
 }
 
-fn add_staff (company: &mut HashMap<String, Vec<String>>, name: String, department: String) {
-
+fn add_staff(company: &mut HashMap<String, Vec<String>>, name: String, department: String) {
     let new_dept: Vec<String> = Vec::new();
     let dept = company.entry(department.clone()).or_insert(new_dept);
 
     dept.push(name);
-
 }
 
-fn get_all_staffs (company: &mut HashMap<String, Vec<String>>) {
+fn remove_staff(company: &mut HashMap<String, Vec<String>>, name: String, department: String) {
+    if check_for_dept(company, department.clone()) {
+        if let Some(staff_list) = company.get_mut(&department) {
+            if let Some(index) = staff_list.iter().position(|staff| *staff == name) {
+                staff_list.remove(index);
+            } else {
+                println!("{} is not in {} department", name, department);
+            }
+        }
+    } else {
+        println!("No such department exists");
+    }
+}
 
+fn get_dept_staff(company: &mut HashMap<String, Vec<String>>, department: String) {
+    if check_for_dept(company, department.clone()) {
+        if let Some(staff_list) = company.get_mut(&department) {
+            staff_list.sort();
+
+            println!("{}", department);
+            println!("-----------");
+
+            for staff in staff_list {
+                println!("{}", staff);
+            }
+        }
+    } else {
+        println!("No such department exists");
+    }
+}
+
+fn check_for_dept(company: &mut HashMap<String, Vec<String>>, department: String) -> bool {
+    let dept = match company.get(&department) {
+        Some(dept) => true,
+        None => false,
+    };
+    dept
+}
+
+fn get_all_staffs(company: &mut HashMap<String, Vec<String>>) {
     for (dept, staff) in company {
         staff.sort();
         println!("{}", dept);
